8d8a9036bce3a964245d51d50592f11a0c90dc24
synodriver <diguohuangjiajinweijun@gmail.com>
2026-06-12 00:31:05 +0800

    fix deref

--- components/ld2460/number/detect_distance_number.cpp
+++ components/ld2460/number/detect_distance_number.cpp
@@ -4,27 +4,48 @@ namespace esphome::ld2460 {
 
 void DetectDistanceNumber::control(float value) {
   this->publish_state(value);
-  this->parent_->set_detect_range(this->parent_->detect_distance_number_->state,
-                                  (int16_t) this->parent_->detect_start_angle_number_->state,
-                                  (int16_t) this->parent_->detect_end_angle_number_->state);
+  this->parent_->set_detect_range(
+      (this->parent_->detect_distance_number_ != nullptr && this->parent_->detect_distance_number_->has_state())
+          ? this->parent_->detect_distance_number_->state
+          : this->parent_->detect_distance_,
+      (this->parent_->detect_start_angle_number_ != nullptr && this->parent_->detect_start_angle_number_->has_state())
+          ? this->parent_->detect_start_angle_number_->state
+          : this->parent_->detect_start_angle_,
+      (this->parent_->detect_end_angle_number_ != nullptr && this->parent_->detect_end_angle_number_->has_state())
+          ? this->parent_->detect_end_angle_number_->state
+          : this->parent_->detect_end_angle_);
   this->parent_->get_detect_range();
   this->parent_->save_to_flash();
 }
 
 void DetectStartAngleNumber::control(float value) {
   this->publish_state(value);
-  this->parent_->set_detect_range(this->parent_->detect_distance_number_->state,
-                                  (int16_t) this->parent_->detect_start_angle_number_->state,
-                                  (int16_t) this->parent_->detect_end_angle_number_->state);
+  this->parent_->set_detect_range(
+      (this->parent_->detect_distance_number_ != nullptr && this->parent_->detect_distance_number_->has_state())
+          ? this->parent_->detect_distance_number_->state
+          : this->parent_->detect_distance_,
+      (this->parent_->detect_start_angle_number_ != nullptr && this->parent_->detect_start_angle_number_->has_state())
+          ? this->parent_->detect_start_angle_number_->state
+          : this->parent_->detect_start_angle_,
+      (this->parent_->detect_end_angle_number_ != nullptr && this->parent_->detect_end_angle_number_->has_state())
+          ? this->parent_->detect_end_angle_number_->state
+          : this->parent_->detect_end_angle_);
   this->parent_->get_detect_range();
   this->parent_->save_to_flash();
 }
 
 void DetectEndAngleNumber::control(float value) {
   this->publish_state(value);
-  this->parent_->set_detect_range(this->parent_->detect_distance_number_->state,
-                                  (int16_t) this->parent_->detect_start_angle_number_->state,
-                                  (int16_t) this->parent_->detect_end_angle_number_->state);
+  this->parent_->set_detect_range(
+      (this->parent_->detect_distance_number_ != nullptr && this->parent_->detect_distance_number_->has_state())
+          ? this->parent_->detect_distance_number_->state
+          : this->parent_->detect_distance_,
+      (this->parent_->detect_start_angle_number_ != nullptr && this->parent_->detect_start_angle_number_->has_state())
+          ? this->parent_->detect_start_angle_number_->state
+          : this->parent_->detect_start_angle_,
+      (this->parent_->detect_end_angle_number_ != nullptr && this->parent_->detect_end_angle_number_->has_state())
+          ? this->parent_->detect_end_angle_number_->state
+          : this->parent_->detect_end_angle_);
   this->parent_->get_detect_range();
   this->parent_->save_to_flash();
 }
